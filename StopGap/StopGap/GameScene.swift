ac2b3f3fbd6a21b64b86f63b0e94f84819ea46e0
cambardell <cambardell3@gmail.com>
2016-01-14 20:23:08 -0500

    Minor

--- StopGap/StopGap/GameScene.swift
+++ StopGap/StopGap/GameScene.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  GameScene.swift
 //  StopGap
 //
@@ -9,20 +9,53 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var rampMan = SKSpriteNode()
+
     override func didMoveToView(view: SKView) {
-    
+        addMan(CGPointMake(frame.size.width/10, frame.size.height/2))
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
         for touch in touches {
-            let location = touch.locationInNode(self)
-            
+            let touchposition = touch.locationInNode(self)
+            if touchposition.y > 512 {
+                moveMan(1)
+            }
+            if touchposition.y < 512 && touchposition.y > 256 {
+                moveMan(2)
+            }
+            if touchposition.y < 256 {
+                moveMan(3)
+            }
         }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    func addMan (position: CGPoint) {
+        // returns a new man
+        rampMan = RampMan(position: position)
+        self.addChild(rampMan)
+
+    }
+    
+    func moveMan(lane: Int) {
+        switch lane {
+        case 1:
+            print("one")
+            rampMan.position.y = 640
+        case 2:
+            print("two")
+            rampMan.position.y = 384
+        case 3:
+            print("three")
+            rampMan.position.y = 128
+        default:
+             break
+        }
+        
     }
 }
---
+++ StopGap/StopGap/RampMan.swift
@@ -0,0 +1,33 @@
+//
+//  RampMan.swift
+//  StopGap
+//
+//  Created by Cameron Bardell on 2016-01-14.
+//  Copyright © 2016 Cameron Bardell. All rights reserved.
+//
+
+import SpriteKit
+
+class RampMan: SKSpriteNode {
+    var laneOne:Bool
+    var laneTwo:Bool
+    var laneThree:Bool
+    init(position: CGPoint) {
+        let texture = SKTexture(imageNamed: "RampMan")
+        self.laneOne = false
+        self.laneTwo = true
+        self.laneThree = false
+          super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
+        self.position = position
+        
+
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.laneOne = false
+        self.laneTwo = true
+        self.laneThree = false
+        super.init(coder: aDecoder)
+        
+    }
+}
