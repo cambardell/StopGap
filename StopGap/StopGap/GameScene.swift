7fbae8787773fb878f8ba7190678e6da788f4abc
cambardell <cambardell3@gmail.com>
2016-01-16 20:26:30 -0500

    Added lane lines, fixed orientation, minor changes

--- StopGap/StopGap/GameScene.swift
+++ StopGap/StopGap/GameScene.swift
@@ -10,22 +10,36 @@ import SpriteKit
 
 class GameScene: SKScene {
     var rampMan = SKSpriteNode()
+    var line = SKShapeNode()
+    var line2 = SKShapeNode()
 
     override func didMoveToView(view: SKView) {
         addMan(CGPointMake(frame.size.width/10, frame.size.height/2))
+        line = SKShapeNode(rectOfSize: CGSize(width: 1, height: frame.size.height))
+        line.fillColor = SKColor.blackColor()
+        line.strokeColor = SKColor.blackColor()
+        line.lineWidth = 0.1
+        line.position = CGPoint(x: frame.size.width/1.5, y: frame.size.height/2)
+        addChild(line)
+        line2 = SKShapeNode(rectOfSize: CGSize(width: 1, height: frame.size.height))
+        line2.fillColor = SKColor.blackColor()
+        line2.strokeColor = SKColor.blackColor()
+        line2.position = CGPoint(x: frame.size.width/3, y: frame.size.height/2)
+        addChild(line2)
+
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         for touch in touches {
             let touchposition = touch.locationInNode(self)
-            if touchposition.y > 512 {
+            if touchposition.x > frame.size.width/1.5 {
                 moveMan(1)
             }
-            if touchposition.y < 512 && touchposition.y > 256 {
+            if touchposition.x < frame.size.width/1.5 && touchposition.x > frame.size.width/3 {
                 moveMan(2)
             }
-            if touchposition.y < 256 {
+            if touchposition.x < frame.size.width/3 {
                 moveMan(3)
             }
         }
@@ -46,13 +60,13 @@ class GameScene: SKScene {
         switch lane {
         case 1:
             print("one")
-            rampMan.position.y = 640
+            rampMan.position.x = frame.size.width/1.2
         case 2:
             print("two")
-            rampMan.position.y = 384
+            rampMan.position.x = frame.size.width/2
         case 3:
             print("three")
-            rampMan.position.y = 128
+            rampMan.position.x = frame.size.width/6
         default:
              break
         }
